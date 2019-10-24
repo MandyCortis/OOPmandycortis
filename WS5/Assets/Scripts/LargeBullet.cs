bce6c73b7c68d1eed265076a1fe47c8739263381
MandyCortis <56277080+MandyCortis@users.noreply.github.com>
2019-10-24 15:05:11 +0200

    WS5

---
+++ WS5/Assets/Scripts/LargeBullet.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LargeBullet : Bullet //inheritance with Bullet (parent)
+{
+    // Start is called before the first frame update
+    protected override void Start()
+    {
+        base.Start();
+        speed = -3;
+    }
+
+    // Update is called once per frame
+    protected override void Update()
+    {
+        base.Update();
+    }
+}
